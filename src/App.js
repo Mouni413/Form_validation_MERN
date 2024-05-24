917b04010e036b2955e8049aad4b9d55c95f3992
Mouni413 <129064482+Mouni413@users.noreply.github.com>
2024-05-24 19:49:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import TimeLine from "./components/TimeLine";
+import Footer from "./components/Footer";
+import WorkSection from "./components/WorkSection";
+
+function App() {
+  return (
+    <>
+      <WorkSection />
+      <TimeLine />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
